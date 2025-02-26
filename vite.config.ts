2bd3f9c2eb73b0dfa7d8431064a46669e721828e
tensormvjce <tensormvjce@gmail.com>
2025-02-26 22:40:05 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   build: {
     outDir: 'dist',
     sourcemap: true
-  }
+  },
+  server: {
+  },
+  base: '/',
+  publicDir: 'public',
 });
